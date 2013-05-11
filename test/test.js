086d087743b13c1cb02998a7337418831dc52b41
Alex J Burke <alex@alexjeffburke.com>
2013-05-11 15:15:07 +0200

    Test for and fixup init/clone of repos at a specific path.
    Add tests for the relocation of repositories. While this worked for clone,
    catch a bug with init where git expected a single argument but we suplied
    more than one.

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@
 
 // imports
 var fs = require('fs');
+var path = require('path');
 
 // local imports
 var Git = require('../lib/git-exec');
@@ -14,20 +15,27 @@ var rimraf = require('rimraf');
 
 // constants
 var TEST_REPO = 'test_repo';
+var TEST_PATH = 'test';
 
 // node <0.8 compat
-var exists = fs.exists || require('path').exists;
+var exists = fs.exists || path.exists;
 
 module.exports = exports = {
   tearDown: function (callback) {
+    // delete any directory in the basic location
     rimraf(TEST_REPO, function() {
-      callback();
+      // delete any directory present at the modified path
+      rimraf(path.join(TEST_PATH, TEST_REPO), function() {
+        callback();
+      });
     });
   }
 };
 
-function checkRepoObject(test, repo) {
-  exists(TEST_REPO, function(doesExist) {
+function checkRepoObject(test, repo, path) {
+  path = path || TEST_REPO;
+
+  exists(path, function(doesExist) {
     test.ok(doesExist, 'test repository was created');
     test.ok(repo, 'repository object created');
     test.ok(repo instanceof Git, 'repository object has correct type');
@@ -43,11 +51,31 @@ exports.testClone = function(test) {
   });
 };
 
+exports.testCloneAtPath = function(test) {
+  var self = this;
+  var repo_path = path.join(TEST_PATH, TEST_REPO);
+
+  Git.clone('.', repo_path, function(repo) {
+    checkRepoObject(test, repo, repo_path);
+    test.done();
+  });
+};
+
 exports.testInit = function(test) {
   var self = this;
 
   Git.init(TEST_REPO, null, function(repo) {
     checkRepoObject(test, repo);
+    test.done();
+  });
+};
+
+exports.testInitAtPath = function(test) {
+  var self = this;
+  var repo_path = path.join(TEST_PATH, TEST_REPO);
+
+  Git.init(TEST_REPO, TEST_PATH, function(repo) {
+    checkRepoObject(test, repo, repo_path);
     test.done();
   });
 };
